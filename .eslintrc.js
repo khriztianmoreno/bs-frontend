e45935236ddf6226d8df8be609850b473b8542a1
khriztianmoreno <khriztianmoreno@gmail.com>
2021-01-30 18:01:42 -0500

    Code base

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    semi: 0,
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', 'ts', 'tsx'] },
+    ],
+    'import/extensions': 0,
+  },
+}
---
+++ src/components/atoms/Navbar.tsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled'
+
+import logo from '../../assets/img/logo.png'
+
+const Container = styled.nav`
+  align-items: center;
+  background: rgb(35, 42, 66);
+  display: flex;
+  height: 10vh;
+  justify-content: center;
+  width: 100%;
+`
+
+const Navbar = () => (
+  <Container>
+    <p>
+      <img src={logo} alt="Bunny studio logo" />
+    </p>
+  </Container>
+)
+
+export default Navbar
